8fccc2755b9a4944a67bfefb754b735a479c0f88
Mika Leppiaho <mika.leppiaho@gmail.com>
2020-06-15 21:06:10 +0300

    added a test user

---
+++ build/precache-manifest.449d267850b5a998059ee319592d8e1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2895edb8167ba294518b50b46179b41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef8d3627d86a149f07bb",
+    "url": "/static/css/main.7cec6e52.chunk.css"
+  },
+  {
+    "revision": "136f262cfce296c96e4f",
+    "url": "/static/js/2.d72fe449.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d72fe449.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef8d3627d86a149f07bb",
+    "url": "/static/js/main.9c810d56.chunk.js"
+  },
+  {
+    "revision": "350730a92ae41426dbaf",
+    "url": "/static/js/runtime-main.8bd7b2ce.js"
+  }
+]);
