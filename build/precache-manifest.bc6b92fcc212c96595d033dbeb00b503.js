9ba43aaed687ec22abe2229bcd0dbeddddf78bd9
Mika Leppiaho <mika.leppiaho@gmail.com>
2020-06-15 20:59:00 +0300

    added list sorting

---
+++ build/precache-manifest.bc6b92fcc212c96595d033dbeb00b503.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b3de656dd00b66bdfa11de1b8b31c88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64edaf0737d1417a0c40",
+    "url": "/static/css/main.7cec6e52.chunk.css"
+  },
+  {
+    "revision": "136f262cfce296c96e4f",
+    "url": "/static/js/2.d72fe449.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d72fe449.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64edaf0737d1417a0c40",
+    "url": "/static/js/main.33814c1d.chunk.js"
+  },
+  {
+    "revision": "350730a92ae41426dbaf",
+    "url": "/static/js/runtime-main.8bd7b2ce.js"
+  }
+]);
